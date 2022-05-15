1c374211a0f4edda5807bd8a7f419dabdabde2aa
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-05-15 14:16:53 +0300

    add

--- 000-prologue/001-start/002-scope-shadwing-more/src/main.rs
+++ 000-prologue/001-start/002-scope-shadwing-more/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
+    /*
+      including broken code for
+      testing/debuging purpose..
+    */
     let x:i32 = 459;
+    let mut y:i32 = 2323;
+    let g: i32 = 99;
     {
         let xx:i32 = x * 2;
+        let g = 97;
         println!("x: {} and xx: {}",x,xx);
+        assert_eq!(g,88);
     }
+    y = "here is my string";
+    println!("{}",y);
+    assert_eq!(g,234);
+
+    let g = 76;
+    println!("{}",g);
     my_func();
 }
 
