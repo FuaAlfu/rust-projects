8b68332b15c14248b13e30b14e35a12531348709
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-11-24 23:34:17 +0300

    add

--- 003-rust-crud-firebase/src/main.rs
+++ 003-rust-crud-firebase/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use firebase_rs::*;
 use serde::{Deserialize, Serialize};
 
@@ -21,5 +22,40 @@ async fn main(){
         email: "alfuGun@foo.com".to_string(),
     };
 
+    //add the link of app, from your firebase account
     let firebase = Firebase::new("");
+
+    let response = set_user(&firebase, &user.await);
+    let mut user = get_user(&firebase, &response.name.await);
+    println!("{:?}",user);
+
+    let users = get_users(&firebase).await;
+    println!("{:?}",users);
+
+    user.email = "updated.mail@gmail.com".to_string();
+    let updated_user = updated_user(&firebase, &response.name, &user).await;
+    println!("{:?}",updated_user);
+
+    delete_user(&firebase, &response.name).await;
+    println!("User deleted..");
+}
+
+async fn set_user() -> Response{}
+
+async fn get_users() -> HashMap<String, User>{}
+
+async fn set_user() -> User{}
+
+async fn update_user() -> User{}
+
+async fn delete_user(){}
+
+//convert a string to a response
+fn string_to_response(s: &str) -> Response{
+    serde_json::from_str(s).unwarp()
+}
+
+//convert a string to a user
+fn string_to_user(s: &str) -> User{
+    serde_json::from_str(s).unwarp()
 }
